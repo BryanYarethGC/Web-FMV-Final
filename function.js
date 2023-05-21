848ac9fed7ea1db552f465e15c03612f743d7dfc
FernandaGarciaCamacho <126592924+FernandaGarciaCamacho@users.noreply.github.com>
2023-05-21 16:04:01 -0600

    Update function.js

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ var x=0
 function menu(){
     if(x==0){
         x=1;
-        txt="<div class='menu'><a class='boton' href=''><p>NOTICIAS</p></a><a class='boton' href=''><p>CONTACTO</p></a><a class='boton' href=''><p>AYUDA</p></a><a class='boton' href=''><p>TEST VOCACIONAL</p></a></div>" 
+        txt="<div class='menu'><a class='boton' href=''><p>NOTICIAS</p></a><a class='boton' href=''><p>CONTACTO</p></a><a class='boton' href='Contacto.html'><p>AYUDA</p></a><a class='boton' href=''><p>TEST VOCACIONAL</p></a></div>" 
     }else{
         if(x==1){
             x=0;
